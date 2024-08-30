3d5154f99cbbe67803f4df27d61d9cd33235716a
jangyonghan <wkddydgks1@gmail.com>
2024-08-30 17:14:19 +0900

    테일윈드 설정 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,12 @@
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-// export default config;
-
+import flowbite from "flowbite-react/tailwind";
 import type { Config } from "tailwindcss";
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = withMT({
-  corePlugins: {
-    preflight: true,
-  },
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -67,7 +42,6 @@ const config: Config = withMT({
       },
     },
   },
-  plugins: [],
-});
-
+  plugins: [require("flowbite/plugin")],
+};
 export default config;
